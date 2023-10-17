2510715b06cb82b24591b3484d455cdca2bae6c5
Layla Hammoud <laylahammoud63@gmail.com>
2023-10-17 16:05:28 +0300

    Finish Ex 2

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //Exercice 1 A
 const SeekAndSwap = (array) => {
-  const startingTime = performance.now()
+  const startingTime = performance.now();
   for (let i = 0; i < array.length; i++) {
     for (let j = i + 1; j < array.length; j++) {
       if (array[j] < array[i]) {
@@ -10,28 +10,45 @@ const SeekAndSwap = (array) => {
       }
     }
   }
-  const EndingTime = performance.now()
-  const timeTaken = EndingTime - startingTime
-  console.log(timeTaken)
+  const EndingTime = performance.now();
+  const timeTaken = EndingTime - startingTime;
+  console.log(timeTaken);
   return array;
 };
-console.log(SeekAndSwap([3, 5, 4, 21, 0, -1]))
+// console.log(SeekAndSwap([3, 5, 4, 21, 0, -1]))
 
 //Exercice 1 B
 const SlideToLeft = (array) => {
-  const startingTime = performance.now()
+  const startingTime = performance.now();
   for (let i = 1; i < array.length; i++) {
-      for (let j = i; j > 0; j--) {
-        if (array[j] < array[j - 1]) {
-          let temp = array[j];
-          array[j] = array[j-1];
-          array[j-1] = temp;
-        }
+    for (let j = i; j > 0; j--) {
+      if (array[j] < array[j - 1]) {
+        let temp = array[j];
+        array[j] = array[j - 1];
+        array[j - 1] = temp;
       }
+    }
   }
-  const EndingTime = performance.now()
-  const timeTaken = EndingTime - startingTime
-  console.log(timeTaken)
+  const EndingTime = performance.now();
+  const timeTaken = EndingTime - startingTime;
+  console.log(timeTaken);
   return array;
 };
-console.log(SlideToLeft([3, 5, 4, 21, 0, -1]));
+// console.log(SlideToLeft([3, 5, 4, 21, 0, -1]));
+
+const generateRandomNumber = () => {
+  return Math.floor(Math.random() * 10);
+};
+//Exercice 2
+const initialize2DArray = () => {
+  let array = [];
+  for (let i = 0; i < 3; i++) {
+    array.push([]);
+    for (let j = 0; j < 5; j++) {
+      array[i].push(generateRandomNumber());
+    }
+    console.log(`row ${i+1} -> ${array[i]}`);
+  }
+  return array;
+};
+console.log(initialize2DArray());
